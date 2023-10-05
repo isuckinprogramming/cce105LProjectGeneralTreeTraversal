5a750bbbb01c302730bd068f7365f68c92c9fb74
Borgs <nisiyawa55@gmail.com>
2023-10-04 19:24:46 -0700

    Fix: Corrected general tree creation
    Added the wrong general tree node to the wrong children node container.
    Fixed it by specifying which of the children of the root node will
    contain the level 2 children nodes.
    
    Working, program now shows which node is visited and indicate when
    a node is found.

--- src/generalTreeImplementation/GeneralTree.java
+++ src/generalTreeImplementation/GeneralTree.java
@@ -3,27 +3,51 @@ package src.generalTreeImplementation;
 
 public class GeneralTree {
   
+  GeneralTreeNode rootNode;
   public GeneralTree() {
 
     long timeStart = System.currentTimeMillis();
 
-    GeneralTreeNode rootNode = new GeneralTreeNode(1);
-  
+    rootNode = new GeneralTreeNode(1);
+
     rootNode.children[0] = new GeneralTreeNode(2);
     rootNode.children[1] = new GeneralTreeNode(3);
     rootNode.children[2] = new GeneralTreeNode(4);
 
-    rootNode.children[0] = new GeneralTreeNode(5);    
-    rootNode.children[0] = new GeneralTreeNode(6);
-    rootNode.children[0] = new GeneralTreeNode(7);
+    rootNode.children[1].children[0] = new GeneralTreeNode(5);
+    rootNode.children[1].children[1] = new GeneralTreeNode(6);
+    rootNode.children[1].children[2] = new GeneralTreeNode(7);
 
     long timeEnd = System.currentTimeMillis();
 
-    System.out.println( timeEnd - timeStart);
+    System.out.println(timeEnd - timeStart);
 
+  }
+  
+  private static void checkNodeDistanceOfTwoNodes(GeneralTreeNode rootNode, int a, int b,boolean isProcessFinished) {
+
+    if(rootNode == null || !isProcessFinished ){
+      return;
+    }
+    System.out.println("VISITED : " + rootNode.dataInside);
+    for (GeneralTreeNode childNode : rootNode.children) {
+      
+      if (childNode == null) { continue; }
+      System.out.println("VISITED : " + childNode.dataInside);
+
+
+      if (childNode.dataInside == a) {
+        System.out.println("found start");
+      } else if (childNode.dataInside == b) {
+        System.out.println("found end ");
+        checkNodeDistanceOfTwoNodes(childNode, a, b, false);
+      }
+      checkNodeDistanceOfTwoNodes(childNode, a, b, true);
+    }
   }
 
   public static void main(String[] args) {
     GeneralTree test = new GeneralTree();
+    checkNodeDistanceOfTwoNodes(test.rootNode, 3, 6,true);
   }
 }
