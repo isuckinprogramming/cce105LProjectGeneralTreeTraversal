0cd8835ad8151eb03149113c71ae0d63b53a0b07
Borgs <nisiyawa55@gmail.com>
2023-10-04 21:25:08 -0700

    Created function to traverse Tree
    Created a function to traverse the general tree using the
    GeneralTree nodes and presenting the details for the traversal
    in the console.

--- src/generalTreeImplementation/GeneralTree.java
+++ src/generalTreeImplementation/GeneralTree.java
@@ -4,50 +4,115 @@ package src.generalTreeImplementation;
 public class GeneralTree {
   
   GeneralTreeNode rootNode;
+
   public GeneralTree() {
 
     long timeStart = System.currentTimeMillis();
 
-    rootNode = new GeneralTreeNode(1);
+    // NODE CREATION - RECREATION OF THE GENERAL TREE FROM THE PROBLEM 
+    rootNode = new GeneralTreeNode(1, 0);
 
-    rootNode.children[0] = new GeneralTreeNode(2);
-    rootNode.children[1] = new GeneralTreeNode(3);
-    rootNode.children[2] = new GeneralTreeNode(4);
+    rootNode.children[0] = new GeneralTreeNode(2, 1);
+    rootNode.children[1] = new GeneralTreeNode(3, 1);
+    rootNode.children[2] = new GeneralTreeNode(4, 1);
 
-    rootNode.children[1].children[0] = new GeneralTreeNode(5);
-    rootNode.children[1].children[1] = new GeneralTreeNode(6);
-    rootNode.children[1].children[2] = new GeneralTreeNode(7);
+    rootNode.children[1].children[0] = new GeneralTreeNode(5, 2);
+    rootNode.children[1].children[1] = new GeneralTreeNode(6, 2);
+    rootNode.children[1].children[2] = new GeneralTreeNode(7, 2);
 
     long timeEnd = System.currentTimeMillis();
-
-    System.out.println(timeEnd - timeStart);
+    long creationTime = timeEnd - timeStart;
+    
+    System.out.println("General tree creation time: (Millis) " + creationTime);
 
   }
   
-  private static void checkNodeDistanceOfTwoNodes(GeneralTreeNode rootNode, int a, int b,boolean isProcessFinished) {
+  // this function will not work. kind of sucks
+  private static void printOutTree(GeneralTree tree) {
 
-    if(rootNode == null || !isProcessFinished ){
+    printNode(tree.rootNode,0);
+    
+    printNode(tree.rootNode.children[0],0);
+    printNode(tree.rootNode.children[1],0);
+    
+    printNode(tree.rootNode.children[1].children[0],1);
+    printNode(tree.rootNode.children[1].children[1],1);
+    printNode(tree.rootNode.children[1].children[2],1);
+
+    printNode(tree.rootNode.children[2],1);
+
+  }
+  
+  private static void printNode(GeneralTreeNode node, int heightStartPadding) {
+    String padding = "";
+    String prePadding = "|___";
+    for (int i = 1; i < node.height; i++) {
+      padding += (i < heightStartPadding) ? "   " : "___";
+    }
+
+    System.out.println(padding + node.dataInside);
+  }
+
+  // variables used in checking distance of two nodes 
+  private static int nodesTraversed = 0;
+  private static boolean isStartFound = false;
+  private static long traverseStartTime = 0;
+  private static long traverseEndTime = 0;
+  /***
+   * Traversing the tree in an in-order fashion, starting from 
+   * the first child of the children node.
+   * 
+   * @param rootNode node to check for value, children of this node will be traversed for 
+   * finding the values of the start and the end.
+   * @param a number of the starting node.
+   * @param b number of the end node.
+  */ 
+  private static void checkNodeDistanceOfTwoNodes(GeneralTreeNode rootNode, int a, int b) {
+
+    if (rootNode == null) {
       return;
     }
-    System.out.println("VISITED : " + rootNode.dataInside);
+    
+    nodesTraversed += (isStartFound) ? 1: 0; 
+    String message = "VISITED : " + rootNode.dataInside;
+    
+    if (rootNode.dataInside == a) {
+      isStartFound = true;
+      message += " <-- : found start";
+      traverseStartTime = System.currentTimeMillis();
+    } 
+    else if (rootNode.dataInside == b) {
+      
+      traverseEndTime = System.currentTimeMillis();
+      message += " <-- found end : nodes traversed = " + nodesTraversed + " time in millis : " + (traverseEndTime - traverseStartTime); 
+      
+      // return the default values of the function variables
+      
+      nodesTraversed = 0;
+      System.out.println(message);
+      
+      traverseEndTime = 0;
+      traverseStartTime = 0;
+      isStartFound = false;
+
+      // end function -- RECURSION BASE CASEs
+      return;
+    }
+    System.out.println( message );
+    // traverse children if found no match between data
     for (GeneralTreeNode childNode : rootNode.children) {
       
-      if (childNode == null) { continue; }
-      System.out.println("VISITED : " + childNode.dataInside);
-
-
-      if (childNode.dataInside == a) {
-        System.out.println("found start");
-      } else if (childNode.dataInside == b) {
-        System.out.println("found end ");
-        checkNodeDistanceOfTwoNodes(childNode, a, b, false);
-      }
-      checkNodeDistanceOfTwoNodes(childNode, a, b, true);
+      checkNodeDistanceOfTwoNodes(childNode, a, b);
     }
   }
 
   public static void main(String[] args) {
     GeneralTree test = new GeneralTree();
-    checkNodeDistanceOfTwoNodes(test.rootNode, 3, 6,true);
+
+    checkNodeDistanceOfTwoNodes(test.rootNode, 3, 6);
+    
+    checkNodeDistanceOfTwoNodes(test.rootNode, 1, 6);
+
+    checkNodeDistanceOfTwoNodes(test.rootNode, 2, 4);
   }
 }
--- src/generalTreeImplementation/GeneralTreeNode.java
+++ src/generalTreeImplementation/GeneralTreeNode.java
@@ -2,11 +2,12 @@ package src.generalTreeImplementation;
 
 public class GeneralTreeNode {
   
-  public GeneralTreeNode(int data) {
+  public GeneralTreeNode(int data, int height) {
     dataInside = data;
+    this.height = height; 
   }
 
-  
+  public int height = 0;
   public int dataInside = -1;
   
   public GeneralTreeNode[] children = {
