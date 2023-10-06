f9896ed5c7eb307c591c11bda9e6870762a91ba3
Borgs <nisiyawa55@gmail.com>
2023-10-05 18:09:12 -0700

    Working : Finding Shortest Distance Between nodes
    Added a new method to find the shortest distance between two nodes.
    Generated by ChatGPT, methods used/referenced by ChatGPT have some
    alterations inside the class.
    
    I renamed some variables to be more descriptive, longer and more verbose
    than what is needed but I really need it to understand exactly what is
    going on.
    
    The tree generated for testing purposes is not from user input, the
    nodes to be searched for within the tree are the only ones which are
    from user.

--- src/generalTreeImplementation/GeneralTreeCreationReference.java
+++ src/generalTreeImplementation/GeneralTreeCreationReference.java
@@ -15,10 +15,14 @@ class TreeNodeForGeneralTreeCreation {
     private int parentData;
     private int nodeData;
     private int depth;
-
+    private TreeNodeForGeneralTreeCreation refToParent;
     private List<TreeNodeForGeneralTreeCreation> children;
 
-    public TreeNodeForGeneralTreeCreation(int parentData, int insideDataForNode, int depth) {
+    public TreeNodeForGeneralTreeCreation(
+      int parentData,
+      int insideDataForNode,
+      int depth,
+      TreeNodeForGeneralTreeCreation parentRef ) {
         
       this.parentData = parentData;
       this.nodeData = insideDataForNode;
@@ -46,19 +50,59 @@ class TreeNodeForGeneralTreeCreation {
 }
 
 public class GeneralTreeCreationReference {
-    private TreeNodeForGeneralTreeCreation root;
+
+
+  public static void main(String[] args) {
+
+    testingShortestPath();
+  }
+  
+  public static void testingShortestPath() {
+
+    GeneralTreeCreationReference tree = new GeneralTreeCreationReference(1); // Creating a tree with root data 1
+
+    addTestNodesInsideTree(tree);
+
+    getStringRepresentation(tree.root);
+    String input = JOptionPane
+        .showInputDialog("GENERAL TREE VISUALIZATION\n" + stringRepresentingTree + "\nInput number to find path inside tree: ");
+
+    String[] numbersRaw = input.split(" ");
+
+    int[] numToFind = { Integer.parseInt(numbersRaw[0]), Integer.parseInt(numbersRaw[1]) };
+
+    // List<Integer> shortestPath = tree.shortestPathBetweenTwoNodes( numToFind[0], numToFind[1]  );
+
+    String outPutMessage = 
+      "Shortest Path from : " + numToFind[0] + " and " + numToFind[1] + " is "
+      + tree.shortestDistance(numToFind[0], numToFind[1])
+      + "\n" + stringRepresentingTree;
+      
+    JOptionPane.showMessageDialog(null, outPutMessage);
+  }
+
+
+
+  private TreeNodeForGeneralTreeCreation root;
 
     public GeneralTreeCreationReference(int rootData) {
-        root = new TreeNodeForGeneralTreeCreation(-1, rootData, 0); // Assuming -1 as the parent of the root node
+        root = new TreeNodeForGeneralTreeCreation(-1, rootData, 0, null); // Assuming -1 as the parent of the root node
     }
 
     public void addNode(int parentData, int childData) {
 
+      // get reference to the node that will receive the new child node
       TreeNodeForGeneralTreeCreation parentNode = findNode(root, parentData);
 
       if (parentNode != null) {
 
-        TreeNodeForGeneralTreeCreation childNode = new TreeNodeForGeneralTreeCreation( parentData, childData, parentNode.getDepth() + 1 );
+        TreeNodeForGeneralTreeCreation childNode = new TreeNodeForGeneralTreeCreation(
+          parentData,
+          childData,
+          parentNode.getDepth() + 1,
+          parentNode
+        );
+
         parentNode.addChild(childNode);
 
       } else {
@@ -67,40 +111,102 @@ public class GeneralTreeCreationReference {
       }
     }
 
+    
     /**
      * Use method to find a node that is located as a children within the root node.
-     * @param node
-     * @param data
-     * @return
+     * @param nodeToSearchWithin node to conduct search inside.
+     * @param data data contained by the node to search for within the node to search within
+     * @return Reference to the node with a data inside equal to the data param.
      */
-    private TreeNodeForGeneralTreeCreation findNode(TreeNodeForGeneralTreeCreation node, int data) {
+    private TreeNodeForGeneralTreeCreation findNode(TreeNodeForGeneralTreeCreation nodeToSearchWithin, int data) {
 
-      if (node == null) {
+      if (nodeToSearchWithin == null) {
         return null;
       }
-      if (node.getNodeData() == data) {
-        return node;
+      
+      if (nodeToSearchWithin.getNodeData() == data) {
+        return nodeToSearchWithin;
       }
-      for (TreeNodeForGeneralTreeCreation child : node.getChildren()) {
+
+      for (TreeNodeForGeneralTreeCreation child : nodeToSearchWithin.getChildren()) {
         TreeNodeForGeneralTreeCreation found = findNode(child, data);
         if (found != null) {
           return found;
         }
       }
+      
       return null;
+
     }
 
     
     
-  public List<Integer> shortestPath(int targetData) {
+    public List<Integer> shortestPathFromRooT(int targetData) {
+      List<Integer> path = new ArrayList<>();
+
+      // the rootnode will be used as the base for finding and searching the data
+      // the starting node can be changed but I haven't tested how yet
+      TreeNodeForGeneralTreeCreation refToTargetDestinationNode = findNode(root, targetData);
+
+      // TERMINATION CASE 
+      if (refToTargetDestinationNode == null) {
+
+        return path; // Target node not found in the tree
+      }
+
+      Queue<TreeNodeForGeneralTreeCreation> queue = new LinkedList<>();
+
+      // map reduces the code needed for look up and finding values 
+      Map<TreeNodeForGeneralTreeCreation, TreeNodeForGeneralTreeCreation> parentMap = new HashMap<>();
+
+      queue.offer(root);
+      parentMap.put(root, null);
+
+      // continue loop as long as queue has contents
+      while (!queue.isEmpty()) {
+
+        TreeNodeForGeneralTreeCreation currentNode = queue.poll();
+
+        if (currentNode == refToTargetDestinationNode) {
+
+          // Found the target node, construct the path
+          TreeNodeForGeneralTreeCreation reassignedTargetNode = refToTargetDestinationNode;
+
+          while (reassignedTargetNode != null) {
+
+            path.add(reassignedTargetNode.getNodeData());
+            reassignedTargetNode = parentMap.get(reassignedTargetNode);
+          }
+
+          Collections.reverse(path); // Reverse the path to get root-to-target order
+
+          return path;
+        }
+
+        for (TreeNodeForGeneralTreeCreation child : currentNode.getChildren()) {
+
+          if (!parentMap.containsKey(child)) {
+            queue.offer(child);
+            parentMap.put(child, currentNode);
+          }
+        }
+      }
+
+      return path; // Target node is not reachable from the root
+    }
+
+    public List<Integer> shortestPathBetweenTwoNodes(int startPoint, int endPoint) {
+    
     List<Integer> path = new ArrayList<>();
     
     // the rootnode will be used as the base for finding and searching the data
     // the starting node can be changed but I haven't tested how yet
-    TreeNodeForGeneralTreeCreation targetNode = findNode(root, targetData);
-
+    TreeNodeForGeneralTreeCreation refToTargetDestinationNode = findNode(root, endPoint);
+    
+    TreeNodeForGeneralTreeCreation refToStartNode = findNode(root, startPoint);
+    
     // TERMINATION CASE 
-    if (targetNode == null) {
+    if (refToTargetDestinationNode == null || refToStartNode == null) {
 
       return path; // Target node not found in the tree
     }
@@ -110,8 +216,8 @@ public class GeneralTreeCreationReference {
     // map reduces the code needed for look up and finding values 
     Map<TreeNodeForGeneralTreeCreation, TreeNodeForGeneralTreeCreation> parentMap = new HashMap<>();
 
-    queue.offer(root);
-    parentMap.put(root, null);
+    queue.offer(refToStartNode);
+    parentMap.put(refToStartNode, null);
 
     // continue loop as long as queue has contents
     while (!queue.isEmpty()) {
@@ -119,15 +225,15 @@ public class GeneralTreeCreationReference {
       TreeNodeForGeneralTreeCreation currentNode = queue.poll();
         
         
-      if (currentNode == targetNode) {
+      if (currentNode == refToTargetDestinationNode) {
         
         // Found the target node, construct the path
-        TreeNodeForGeneralTreeCreation node = targetNode;
+        TreeNodeForGeneralTreeCreation reassignedTargetNode = refToTargetDestinationNode;
         
-        while (node != null) {
+        while (reassignedTargetNode != null) {
 
-          path.add(node.getNodeData());
-          node = parentMap.get(node);
+          path.add(reassignedTargetNode.getNodeData());
+          reassignedTargetNode = parentMap.get(reassignedTargetNode);
         }
 
         Collections.reverse(path); // Reverse the path to get root-to-target order
@@ -147,15 +253,47 @@ public class GeneralTreeCreationReference {
     return path; // Target node is not reachable from the root
   }
 
-    public static void main(String[] args) {
+  public int shortestDistance(int nodeData1, int nodeData2) {
 
-      testingShortestPath();
+    TreeNodeForGeneralTreeCreation node1 = findNode(root, nodeData1);
+    TreeNodeForGeneralTreeCreation node2 = findNode(root, nodeData2);
+
+    if (node1 == null || node2 == null) {
+      return -1; // One or both nodes not found in the tree
     }
-    
-    public static void testingShortestPath() {
 
-      GeneralTreeCreationReference tree = new GeneralTreeCreationReference(1); // Creating a tree with root data 1
+    List<Integer> path1 = shortestPathFromRooT(nodeData1);
+    List<Integer> path2 = shortestPathFromRooT(nodeData2);
 
+    if (path1.isEmpty() || path2.isEmpty()) {
+      return -1; // One or both nodes are not reachable from the root
+    }
+
+    // Find the lowest common ancestor between the two nodes
+    int commonAncestor = -1;
+    for (int i = 0; i < Math.min(path1.size(), path2.size()); i++) {
+      
+      Integer path1num = path1.get(i);
+      Integer path2num = path2.get(i);
+      
+      if (!path1num.equals(path2num)) {
+        break;
+      }
+      
+      commonAncestor = path1.get(i);
+    }
+
+    // Calculate the distance by summing the lengths of both paths minus 2
+    // (subtract 2 to remove the common ancestor and avoid double counting)
+    int distance = (path1.size() - 1) + (path2.size() - 1);
+
+    return distance;
+  }
+
+
+
+  public static void addTestNodesInsideTree(GeneralTreeCreationReference tree) {
+  
       tree.addNode(1, 2); // Adding a child node with parent 1 and data 2
 
       tree.addNode(1, 3); // Adding a child node with parent 1 and data 3
@@ -179,30 +317,37 @@ public class GeneralTreeCreationReference {
       tree.addNode(10, 43);
 
       tree.addNode(11, 25);
+  }
 
-      getStringRepresentation(tree.root);
-      String input = JOptionPane
-          .showInputDialog("GENERAL TREE VISUALIZATION\n" + message + "\nInput number to find path inside tree: ");
 
-      int numToFind = Integer.parseInt(input);
-      
-      List<Integer> shortestPath = tree.shortestPath( numToFind );
 
-      String outPutMessage = "Shortest Path to 5: " + shortestPath;
-      outPutMessage += "\n" + message;
-      JOptionPane.showMessageDialog(null, outPutMessage);
-    }
   
-  static String message = "";
+    
+  static String stringRepresentingTree = "";
+
+  /**
+   * This method cannot return a String that represents the 
+   * tree. To access the string representation of the tree. 
+   * Access the variable String tree representation after calling 
+   * this method. 
+   * <br></br>
+   * The method uses recursion which is why the return statement 
+   * cannot be called to return a string value that represents the 
+   * string representation of the tree or the node.
+   * 
+   * @param node Starting node to create a string visualization for the tree 
+  */ 
   public static void getStringRepresentation(TreeNodeForGeneralTreeCreation node) {
     
+    stringRepresentingTree = "";
+    
     String padding = "  ".repeat( node.getDepth() ) + "|__"; // Create padding based on depth
-    message += padding + node.getNodeData() + "\n";
+    stringRepresentingTree += padding + node.getNodeData() + "\n";
    
     for (TreeNodeForGeneralTreeCreation child : node.getChildren()) {
 
       getStringRepresentation( child ); // Recursively concatenate message with the print data of the children of the node.
     }
-    
+    // BASE CASE - Recursion stops when there are no more child nodes to access and call upon this method 
   }
 }
